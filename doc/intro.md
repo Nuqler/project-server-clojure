fdd0682eea74993ab2205081bf188d8421344714
Nuqler <50767779+Nuqler@users.noreply.github.com>
2020-04-01 19:33:45 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to project-server-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/project_server_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns project-server-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [project-server-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
